bc13a79ff2fc55a100c3172f45bb0b3e73a75982
Luc Michalski <michalskiluc79@gmail.com>
2019-06-01 17:15:24 +0200

    add nano bash for dev switch case

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,2 +1,43 @@
-#!/bin/bash
-$@
+#!/bin/sh
+
+echo "case $1"
+
+case "$1" in
+
+  'elk')
+  	ARGS=""
+  	exec twint $@ $ARGS
+	;;
+
+  'tor')
+  	# ARGS="-ip `hostname -i` -dir /data"
+  	# if [ -n "$MASTER_PORT_9333_TCP_ADDR" ] ; then
+	#	ARGS="$ARGS -master.peers=$MASTER_PORT_9333_TCP_ADDR:$MASTER_PORT_9333_TCP_PORT"
+	# fi
+  	exec twint $@ $ARGS
+  	;;
+
+  'ui')
+  	ARGS=""
+  	exec twint $@ $ARGS
+	;;
+
+  'sh')
+  	exec /bin/sh $@
+	;;
+
+  'cronjob')
+	# MASTER=${WEED_MASTER-localhost:9333}
+	# FIX_REPLICATION_CRON_SCHEDULE=${CRON_SCHEDULE-*/7 * * * * *}
+	# echo "$FIX_REPLICATION_CRON_SCHEDULE" 'echo "volume.fix.replication" | weed shell -master='$MASTER > /crontab
+	# BALANCING_CRON_SCHEDULE=${CRON_SCHEDULE-25 * * * * *}
+	# echo "$BALANCING_CRON_SCHEDULE" 'echo "volume.balance -c ALL -f" | weed shell -master='$MASTER >> /crontab
+	echo "Running Crontab:"
+	cat /crontab
+	exec supercronic /crontab
+	;;
+  *)
+	apk add --no-cache nano bash
+  	exec bash
+	;;
+esac
